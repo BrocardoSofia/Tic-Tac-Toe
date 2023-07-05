0648ad6ba7ad13b0fe4b71a235e84ef99bbebe80
BrocardoSofia <sofia.brocardo@hotmail.com>
2023-07-05 00:27:57 -0300

    update now you can play X or O

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,10 +6,12 @@ using UnityEngine.UI;
 public class GameController : MonoBehaviour
 {
     public Text[] buttonList;
+    private string playerSide;
 
     void Awake()
     {
         setGameControllerReferenceOnButtons();
+        playerSide = "X";
     }
 
     void setGameControllerReferenceOnButtons()
@@ -22,11 +24,71 @@ public class GameController : MonoBehaviour
 
     public string getPlayerSide()
     {
-        return "?";
+        return playerSide;
     }
 
     public void endTurn()
     {
-        Debug.Log("EndTurn is not implemented!");
+        if ((buttonList[0].text == playerSide) && (buttonList[1].text == playerSide) && (buttonList[2].text == playerSide))
+        {
+            //if the top row equals the player side
+            gameOver();
+        }
+        else if((buttonList[3].text == playerSide) && (buttonList[4].text == playerSide) && (buttonList[5].text == playerSide))
+        {
+            //if the second row equals the player side
+            gameOver();
+        }
+        else if ((buttonList[6].text == playerSide) && (buttonList[7].text == playerSide) && (buttonList[8].text == playerSide))
+        {
+            //if the third row equals the player side
+            gameOver();
+        }
+        else if ((buttonList[0].text == playerSide) && (buttonList[3].text == playerSide) && (buttonList[6].text == playerSide))
+        {
+            //if the first column equals the player side
+            gameOver();
+        }
+        else if ((buttonList[1].text == playerSide) && (buttonList[4].text == playerSide) && (buttonList[7].text == playerSide))
+        {
+            //if the second column equals the player side
+            gameOver();
+        }
+        else if ((buttonList[2].text == playerSide) && (buttonList[5].text == playerSide) && (buttonList[8].text == playerSide))
+        {
+            //if the third column equals the player side
+            gameOver();
+        }
+        else if ((buttonList[0].text == playerSide) && (buttonList[4].text == playerSide) && (buttonList[8].text == playerSide))
+        {
+            //if the diagonal column equals the player side
+            gameOver();
+        }
+        else if ((buttonList[6].text == playerSide) && (buttonList[4].text == playerSide) && (buttonList[2].text == playerSide))
+        {
+            //if the diagonal column equals the player side
+            gameOver();
+        }
+        else
+        {
+            changePlayerSide();
+        }
+    }
+
+    void gameOver()
+    {
+        //disable all the buttons
+        for (int i = 0; i < buttonList.Length; i++)
+        {
+            buttonList[i].GetComponentInParent<Button>().interactable = false;
+        }
+    }
+
+    void changePlayerSide()
+    {
+        if (playerSide == "X")
+            playerSide = "O";
+        else
+            playerSide = "X";
     }
 }
